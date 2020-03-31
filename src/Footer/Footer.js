4f63c52aa564ec2f412fa1af6a6068f97c80b1a1
Erick Jensen <Erick.Jensen010@gmail.com>
2020-03-31 10:12:58 -0500

    finish base

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <footer>
+                <h1>&copy; React Actors</h1>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
